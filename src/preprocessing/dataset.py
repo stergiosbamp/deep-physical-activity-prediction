72ada8f75f85571c21b0bba2a076393d05d1dac7
stergiosbamp <bampakis.stergios@gmail.com>
2021-10-06 19:42:31 +0300

    Options to save the preprocessed dataset into 'data' directory for rapid ML experiments

--- src/preprocessing/dataset.py
+++ src/preprocessing/dataset.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 from sklearn.model_selection import TimeSeriesSplit
+from pathlib import Path
 
 from .window import SlidingWindow
 from .preprocess import Preprocessor
@@ -8,35 +9,46 @@ from ..data.database import HealthKitDatabase
 
 
 class DatasetBuilder:
-    def __init__(self, n_in):
+    def __init__(self, n_in, save_dataset=True, directory='../../data/df_dataset_all_users.pkl'):
         self.sliding_window = SlidingWindow(n_in=n_in)
         self.hk_database = HealthKitDatabase()
+        self.save_dataset = save_dataset
+        self.directory = Path(directory)
 
     def get_train_test(self, ratio=0.75):
-        users = self.hk_database.get_all_healthkit_users()
 
-        dataset = pd.DataFrame()
-        for user in users[:100]:
-            cursor_results = self.hk_database.get_records_by_user(user_code=user)
-            user_data = list(cursor_results)
-            df_user = pd.DataFrame(user_data)
+        if self.save_dataset and self.directory.exists():
+            dataset = pd.read_pickle(self.directory.__str__())
+        else:
+            users = self.hk_database.get_all_healthkit_users()
 
-            preprocessor = Preprocessor(df=df_user)
-            preprocessor\
-                .remove_outlier_dates()\
-                .resample_dates()\
-                .add_date_features()\
-                # .add_sin_cos_features()
+            dataset = pd.DataFrame()
+            for user in users[:100]:
+                cursor_results = self.hk_database.get_records_by_user(user_code=user)
+                user_data = list(cursor_results)
+                df_user = pd.DataFrame(user_data)
 
-            processed_df = preprocessor.df
+                preprocessor = Preprocessor(df=df_user)
+                preprocessor\
+                    .remove_outlier_dates()\
+                    .resample_dates()\
+                    .add_date_features()\
+                    # .add_sin_cos_features()
 
-            data = self.sliding_window.to_supervised_dataset(processed_df)
+                processed_df = preprocessor.df
 
-            # Append for each user to construct the final dataset
-            dataset = dataset.append(data)
+                data = self.sliding_window.to_supervised_dataset(processed_df)
 
-        # Re-sort based on the dates due to the mix of the different subjects
-        dataset.sort_index(inplace=True)
+                # Append for each user to construct the final dataset
+                dataset = dataset.append(data)
+
+                # Re-sort based on the dates due to the mix of the different subjects
+                dataset.sort_index(inplace=True)
+
+                if self.save_dataset:
+                    # Save dataset into 'data' directory to run ML experiments without
+                    # requiring the whole preprocessing
+                    dataset.to_pickle(self.directory.__str__())
 
         y = dataset['var1(t)']
         X = dataset.drop(columns=['var1(t)'])
