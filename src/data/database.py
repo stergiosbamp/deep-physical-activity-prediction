090b9d2e67a789eaa2272133c535e97bf246d0a0
stergiosbamp <bampakis.stergios@gmail.com>
2021-10-06 18:14:08 +0300

    Add HealthKitDatabase class for common operations to the healthkit data

--- src/data/database.py
+++ src/data/database.py
@@ -19,3 +19,22 @@ class Database:
             collection.insert_many(docs)
         else:
             collection.insert_one(docs)
+
+
+class HealthKitDatabase(Database):
+    HEALTHKIT_STEPSCOUNT_COLLECTION = 'healthkit_stepscount_singles'
+
+    def __init__(self):
+        super().__init__()
+        self.healthkit_collection = self.database.get_collection(self.HEALTHKIT_STEPSCOUNT_COLLECTION)
+
+    def get_all_healthkit_users(self):
+        cursor = self.healthkit_collection.distinct('healthCode')
+        return cursor
+
+    def set_healthkit_index(self, field):
+        self.healthkit_collection.create_index(field)
+
+    def get_records_by_user(self, user_code):
+        cursor = self.healthkit_collection.find({"healthCode": user_code})
+        return cursor
