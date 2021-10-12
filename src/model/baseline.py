8b7d70fa67e0e3bceef15f4ae5f9fe66f8917855
stergiosbamp <bampakis.stergios@gmail.com>
2021-10-12 20:11:07 +0300

    Add in baseline experiment the median absolute error metric. Also keep in mind that LR heavily overfits with date features. Prefer GB, RF or other linear model

--- src/model/baseline.py
+++ src/model/baseline.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, SGDRegressor
 from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
-from sklearn.metrics import r2_score, mean_absolute_percentage_error, mean_absolute_error
+from sklearn.metrics import r2_score, mean_absolute_percentage_error, mean_absolute_error, median_absolute_error
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import MinMaxScaler
 
@@ -12,16 +12,17 @@ from src.preprocessing.dataset import DatasetBuilder
 
 
 if __name__ == '__main__':
-    dataset_builder = DatasetBuilder(n_in=12)
+    dataset_builder = DatasetBuilder(n_in=2*24, save_dataset=False, total_users=10)
 
     X_train, X_test, y_train, y_test = dataset_builder.get_train_test()
 
-    pipe = make_pipeline(MinMaxScaler(), GradientBoostingRegressor(random_state=4))
+    pipe = make_pipeline(MinMaxScaler(), SGDRegressor(random_state=1))
     pipe.fit(X_train, y_train)
     y_pred = pipe.predict(X_test)
     print("r2", r2_score(y_test, y_pred))
     print("mae", mean_absolute_error(y_test, y_pred))
     print("mape", mean_absolute_percentage_error(y_test, y_pred))
+    print("median ae", median_absolute_error(y_test, y_pred))
 
     y_test_val = y_test.values
     x_range = X_test.index
