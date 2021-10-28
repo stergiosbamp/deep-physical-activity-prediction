94ae20b02dd28c016f5bc40ece41c8cc5b63c25b
stergiosbamp <bampakis.stergios@gmail.com>
2021-10-28 18:36:51 +0300

    Re-factored baseline.py as a class with a default pipeline

--- src/model/baseline.py
+++ src/model/baseline.py
@@ -7,30 +7,52 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 from src.preprocessing.dataset import DatasetBuilder
+from src.config.directory import BASE_PATH_HOURLY_DATASETS, BASE_PATH_DAILY_DATASETS
+
+
+class BaselineModel:
+    def __init__(self, x_train, x_test, y_train, y_test):
+        self.x_train = x_train
+        self.x_test = x_test
+        self.y_train = y_train
+        self.y_test = y_test
+
+        self.pipe = make_pipeline(MinMaxScaler(), Ridge(random_state=1))
+        self.y_pred = None
+        self.res = dict()
+
+    def score(self):
+        self.pipe.fit(X_train, y_train)
+        self.y_pred = self.pipe.predict(X_test)
+
+        self.res['r2'] = r2_score(self.y_test, self.y_pred)
+        self.res['mae'] = mean_absolute_error(self.y_test, self.y_pred)
+        self.res['mape'] = mean_absolute_percentage_error(self.y_test, self.y_pred)
+        self.res['median_ae'] = median_absolute_error(self.y_test, self.y_pred)
+
+        return self.res
+
+    def plot_predictions(self):
+        x_range = self.x_test.index
+        plt.plot(x_range, self.y_test.values, label='true')
+        plt.plot(x_range, self.y_pred, label='pred')
+        plt.legend()
+        plt.show()
 
 
 if __name__ == '__main__':
     dataset_builder = DatasetBuilder(n_in=5*24,
                                      granularity='1H',
                                      save_dataset=True,
-                                     directory='../../data/df-5-day-imputed-no-outliers-all-features-all-users-with'
-                                               '-subject-injected.pkl',
+                                     directory='../../data/datasets/daily/df-3-day-imputed-no-outliers-all-features'
+                                               '-all-users-with-subject-injected.pkl',
                                      total_users=None)
 
     X_train, X_test, y_train, y_test = dataset_builder.get_train_test()
 
-    pipe = make_pipeline(MinMaxScaler(), Ridge(random_state=1))
+    baseline_ml = BaselineModel(X_train, X_test, y_train, y_test)
 
-    pipe.fit(X_train, y_train)
-    y_pred = pipe.predict(X_test)
-    print("r2", r2_score(y_test, y_pred))
-    print("mae", mean_absolute_error(y_test, y_pred))
-    print("mape", mean_absolute_percentage_error(y_test, y_pred))
-    print("median ae", median_absolute_error(y_test, y_pred))
+    scores = baseline_ml.score()
+    print(scores)
 
-    y_test_val = y_test.values
-    x_range = X_test.index
-    plt.plot(x_range, y_test_val, label='true')
-    plt.plot(x_range, y_pred, label='pred')
-    plt.legend()
-    plt.show()
+    baseline_ml.plot_predictions()
