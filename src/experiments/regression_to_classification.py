83c37de72ea22e74d4968d1e63bba91a207f385f
stergiosbamp <bampakis.stergios@gmail.com>
2021-10-31 12:49:34 +0200

    In regression to clf add two variations with steps and cyclic features and only steps features

--- src/experiments/regression_to_classification.py
+++ src/experiments/regression_to_classification.py
@@ -10,22 +10,70 @@ from src.preprocessing.dataset import DatasetBuilder
 from src.config.directory import BASE_PATH_VARIATION_DATASETS
 
 
-pipe = make_pipeline(MinMaxScaler(), DecisionTreeClassifier(random_state=1))
+def all_features():
+    pipe = make_pipeline(MinMaxScaler(), DecisionTreeClassifier(random_state=1))
 
-ds_builder = DatasetBuilder(n_in=3 * 24,
-                            granularity='whatever',
-                            save_dataset=True,
-                            directory=os.path.join(BASE_PATH_VARIATION_DATASETS,
-                                                   'df-3*24-classification.pkl'),
-                            classification=True)
+    ds_builder = DatasetBuilder(n_in=3 * 24,
+                                granularity='whatever',
+                                save_dataset=True,
+                                directory=os.path.join(BASE_PATH_VARIATION_DATASETS,
+                                                       'df-3*24-classification.pkl'),
+                                classification=True)
 
-X_train, X_test, y_train, y_test = ds_builder.get_train_test()
+    X_train, X_test, y_train, y_test = ds_builder.get_train_test()
 
-pipe.fit(X_train, y_train)
-y_pred = pipe.predict(X_test)
+    pipe.fit(X_train, y_train)
+    y_pred = pipe.predict(X_test)
+
+    report = classification_report(y_test, y_pred, output_dict=True)
+
+    df = pd.DataFrame.from_dict(report).transpose()
+    df.to_csv('../../results/clf_decision_trees_hourly_classification_all_features.csv', float_format='%.3f')
 
 
-report = classification_report(y_test, y_pred, output_dict=True)
+def steps_cyclic():
+    pipe = make_pipeline(MinMaxScaler(), DecisionTreeClassifier(random_state=1))
 
-df = pd.DataFrame.from_dict(report).transpose()
-df.to_csv('../../results/clf_decision_trees_hourly_classification_all_features.csv', float_format='%.3f')
+    ds_builder = DatasetBuilder(n_in=3 * 24,
+                                granularity='whatever',
+                                save_dataset=True,
+                                directory=os.path.join(BASE_PATH_VARIATION_DATASETS,
+                                                       'df-3*24-classification-steps-cyclic.pkl'),
+                                classification=True)
+
+    X_train, X_test, y_train, y_test = ds_builder.get_train_test()
+
+    pipe.fit(X_train, y_train)
+    y_pred = pipe.predict(X_test)
+
+    report = classification_report(y_test, y_pred, output_dict=True)
+
+    df = pd.DataFrame.from_dict(report).transpose()
+    df.to_csv('../../results/clf_decision_trees_hourly_classification_steps_cyclic.csv', float_format='%.3f')
+
+
+def steps_only():
+    pipe = make_pipeline(MinMaxScaler(), DecisionTreeClassifier(random_state=1))
+
+    ds_builder = DatasetBuilder(n_in=3 * 24,
+                                granularity='whatever',
+                                save_dataset=True,
+                                directory=os.path.join(BASE_PATH_VARIATION_DATASETS,
+                                                       'df-3*24-classification-steps-only.pkl'),
+                                classification=True)
+
+    X_train, X_test, y_train, y_test = ds_builder.get_train_test()
+
+    pipe.fit(X_train, y_train)
+    y_pred = pipe.predict(X_test)
+
+    report = classification_report(y_test, y_pred, output_dict=True)
+
+    df = pd.DataFrame.from_dict(report).transpose()
+    df.to_csv('../../results/clf_decision_trees_hourly_classification_steps_only.csv', float_format='%.3f')
+
+
+if __name__ == '__main__':
+    all_features()
+    steps_cyclic()
+    steps_only()
