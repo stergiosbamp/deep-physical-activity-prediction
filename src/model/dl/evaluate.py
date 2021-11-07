7f4d3327f506fd12b21cb5dc1a9a4b3d51148989
stergiosbamp <bampakis.stergios@gmail.com>
2021-11-07 17:15:28 +0200

    Evaluator class that auto-loads params from checkpoint, that scores the model and plots predictions

--- src/model/dl/evaluate.py
+++ src/model/dl/evaluate.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
 import os
 import torch
-import torch.nn as nn
 
 from sklearn.metrics import mean_absolute_error, r2_score, median_absolute_error
 from sklearn.preprocessing import MinMaxScaler
@@ -10,39 +10,43 @@ from src.config.directory import BASE_PATH_HOURLY_DATASETS
 from src.preprocessing.dataset import DatasetBuilder
 
 
+class Evaluator:
+    def __init__(self, x_test, y_test, model, checkpoint_path):
+        self.x_test = x_test
+        self.y_test = y_test
+        self.model = model.load_from_checkpoint(checkpoint_path)
+        self.y_pred = None
+
+    def evaluate(self):
+        # Important: set it to evaluation mode
+        self.model.eval()
+
+        # Inference from PyTorch model and get predictions as numpy array
+        y_pred = self.model(torch.from_numpy(x_test).float())
+        y_pred = y_pred.detach().numpy()
+
+        self.y_pred = y_pred
+
+        # Now use predictions with any metric from sklearn
+        print("MAE", mean_absolute_error(self.y_test, self.y_pred))
+        print("R2", r2_score(self.y_test, self.y_pred))
+        print("MdAE", median_absolute_error(self.y_test, self.y_pred))
+
+    def plot(self):
+        x_range = self.y_test.index
+        plt.plot(x_range, self.y_test, label='true')
+        plt.plot(x_range, self.y_pred, label='pred')
+        plt.legend()
+        plt.show()
+
+
 if __name__ == '__main__':
-    p = dict(
-        batch_size=128,
-        criterion=nn.L1Loss(),
-        max_epochs=30,
-        n_features=72,
-        hidden_size=100,
-        num_layers=3,
-        dropout=0.2,
-        learning_rate=0.001,
-        num_workers=4
-    )
-
-    # Load PyTorch model
-    loaded = LSTMRegressor.load_from_checkpoint(
-        '../lightning_logs/version_0/checkpoints/epoch=29-step=7559.ckpt',
-        n_features=p['n_features'],
-        hidden_size=p['hidden_size'],
-        batch_size=p['batch_size'],
-        criterion=p['criterion'],
-        num_layers=p['num_layers'],
-        dropout=p['dropout'],
-        learning_rate=p['learning_rate'])
-
-    # Important: set it to evaluation mode
-    loaded.eval()
-
     # Get the dataset to get the same train/test splits
     ds_builder = DatasetBuilder(n_in=3 * 24,
                                 granularity='whatever',
                                 save_dataset=True,
-                                directory=os.path.join('/home/stergios/Development/MSc/Thesis/deep-physical-activity'
-                                                       '-prediction/data/datasets/hourly/df-3*24-imputed-no-outliers-all-features-all-users-with-subject-injected.pkl'))
+                                directory='../../../data/datasets/hourly/df-3*24-imputed-no-outliers-all-features-all'
+                                          '-users-with-subject-injected.pkl')
 
     dataset = ds_builder.create_dataset_steps_features()
     x_train, x_test, y_train, y_test = ds_builder.get_train_test(dataset=dataset)
@@ -52,12 +56,9 @@ if __name__ == '__main__':
     x_train = scaler.fit_transform(x_train)
     x_test = scaler.transform(x_test)
 
-    # Inference from PyTorch model and get predictions as numpy array
-    y_pred = loaded(torch.from_numpy(x_test).float())
-
-    y_pred = y_pred.detach().numpy()
-
-    # Now use predictions with any metric from sklearn
-    print("MAE", mean_absolute_error(y_test, y_pred))
-    print("R2", r2_score(y_test, y_pred))
-    print("MdAE", median_absolute_error(y_test, y_pred))
+    evaluator = Evaluator(x_test=x_test,
+                          y_test=y_test,
+                          model=LSTMRegressor,
+                          checkpoint_path='../lightning_logs/version_6/checkpoints/epoch=9-step=2519.ckpt')
+    evaluator.evaluate()
+    evaluator.plot()
