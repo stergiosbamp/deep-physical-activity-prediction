45abc72069a4e78f739842b1704948dec6a28143
stergiosbamp <bampakis.stergios@gmail.com>
2021-11-25 13:27:42 +0200

    Include MAE in plotting imputation experiment

--- plots/gb_imputation.py
+++ plots/gb_imputation.py
@@ -18,13 +18,21 @@ imputed_median = imputed.loc['median_ae'][0]
 
 median_aes = [not_imputed_median, imputed_median]
 
-fig, ax = plt.subplots(1, 2, figsize=(15, 10))
+# maes
+not_imputed_mae = not_imputed.loc['mae'][0]
+imputed_mae = imputed.loc['mae'][0]
+
+maes = [not_imputed_mae, imputed_mae]
+
+fig, ax = plt.subplots(1, 3, figsize=(15, 10))
 fig.suptitle('Gradient Boosting Regressor for using or not imputation of zeros')
 ax[0].set(ylabel='r2')
 ax[1].set(ylabel='median_ae')
+ax[2].set(ylabel='mae')
 
 x = ['Not imputed', 'Imputed']
 
 sns.barplot(x=x, y=r2s, order=x, hue=x, ax=ax[0])
 sns.barplot(x=x, y=median_aes, order=x, hue=x, ax=ax[1])
+sns.barplot(x=x, y=maes, order=x, hue=x, ax=ax[2])
 plt.show()
