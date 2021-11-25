3bf06357ed21520b272c76ded81deb317e9298e4
stergiosbamp <bampakis.stergios@gmail.com>
2021-11-25 14:18:09 +0200

    Updated classification experiment with new datasets and paths

--- src/experiments/regression_to_classification.py
+++ src/experiments/regression_to_classification.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 
 from sklearn.tree import DecisionTreeClassifier
@@ -7,17 +6,15 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import classification_report
 
 from src.preprocessing.dataset import DatasetBuilder
-from src.config.directory import BASE_PATH_VARIATION_DATASETS
 
 
-def all_features():
+def classification():
     pipe = make_pipeline(MinMaxScaler(), DecisionTreeClassifier(random_state=1))
 
-    ds_builder = DatasetBuilder(n_in=3 * 24,
+    ds_builder = DatasetBuilder(n_in=3*24,
                                 granularity='whatever',
                                 save_dataset=True,
-                                directory=os.path.join(BASE_PATH_VARIATION_DATASETS,
-                                                       'df-3x24-classification.pkl'),
+                                directory='../../data/datasets/variations/df-3x24-clf-just-steps.pkl',
                                 classification=True)
 
     dataset = ds_builder.create_dataset_all_features()
@@ -29,9 +26,9 @@ def all_features():
     report = classification_report(y_test, y_pred, output_dict=True)
 
     df = pd.DataFrame.from_dict(report).transpose()
-    df.to_csv('../../results/classification/clf_decision_trees_hourly_classification_all_features.csv',
+    df.to_csv('../../results/classification/Trees-clf.csv',
               float_format='%.3f')
 
 
 if __name__ == '__main__':
-    all_features()
+    classification()
