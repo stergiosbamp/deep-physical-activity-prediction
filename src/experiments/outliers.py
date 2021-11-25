ca399434ccb69a52f4cb318be0ba021a731dad24
stergiosbamp <bampakis.stergios@gmail.com>
2021-11-25 13:50:06 +0200

    Update outliers experiment with and without outliers case only

--- src/experiments/outliers.py
+++ src/experiments/outliers.py
@@ -10,14 +10,13 @@ from src.preprocessing.dataset import DatasetBuilder
 from src.config.directory import BASE_PATH_VARIATION_DATASETS
 
 
-def removing_upper_outliers():
+def no_outliers():
     pipe = make_pipeline(MinMaxScaler(), GradientBoostingRegressor(verbose=1, random_state=1))
 
-    ds_builder = DatasetBuilder(n_in=3 * 24,
+    ds_builder = DatasetBuilder(n_in=3*24,
                                 granularity='whatever',
                                 save_dataset=True,
-                                directory=os.path.join(BASE_PATH_VARIATION_DATASETS,
-                                                       'df-3x24-imputed-upper-outliers.pkl'))
+                                directory='../../data/datasets/hourly/df-3x24-just-steps.pkl')
 
     dataset = ds_builder.create_dataset_all_features()
     X_train, X_test, y_train, y_test = ds_builder.get_train_test(dataset=dataset)
@@ -31,20 +30,16 @@ def removing_upper_outliers():
 
     # write them to csv
     df = pd.DataFrame.from_dict(results, orient='index')
-    df.to_csv('../../results/outliers/gb_hourly_imputed_upper_outliers.csv')
-
-    # plot
-    baseline_ml.plot_predictions()
+    df.to_csv('../../results/outliers/gb_no_outliers.csv')
 
 
-def removing_all_outliers():
+def with_outliers():
     pipe = make_pipeline(MinMaxScaler(), GradientBoostingRegressor(verbose=1, random_state=1))
 
-    ds_builder = DatasetBuilder(n_in=3 * 24,
+    ds_builder = DatasetBuilder(n_in=3*24,
                                 granularity='whatever',
                                 save_dataset=True,
-                                directory=os.path.join(BASE_PATH_VARIATION_DATASETS,
-                                                       'df-3x24-imputed-both-outliers.pkl'))
+                                directory='../../data/datasets/variations/df-3x24-with-outliers-just-steps.pkl')
 
     dataset = ds_builder.create_dataset_all_features()
     X_train, X_test, y_train, y_test = ds_builder.get_train_test(dataset=dataset)
@@ -58,40 +53,9 @@ def removing_all_outliers():
 
     # write them to csv
     df = pd.DataFrame.from_dict(results, orient='index')
-    df.to_csv('../../results/outliers/gb_hourly_imputed_both_outliers.csv')
-
-    # plot
-    baseline_ml.plot_predictions()
-
-
-def keep_outliers():
-    pipe = make_pipeline(MinMaxScaler(), GradientBoostingRegressor(verbose=1, random_state=1))
-
-    ds_builder = DatasetBuilder(n_in=3 * 24,
-                                granularity='whatever',
-                                save_dataset=True,
-                                directory=os.path.join(BASE_PATH_VARIATION_DATASETS,
-                                                       'df-3x24-imputed-with-outliers.pkl'))
-
-    dataset = ds_builder.create_dataset_all_features()
-    X_train, X_test, y_train, y_test = ds_builder.get_train_test(dataset=dataset)
-
-    baseline_ml = BaselineModel(X_train, X_test, y_train, y_test)
-    baseline_ml.set_pipe(pipe)
-
-    # record
-    results = baseline_ml.score()
-    print(results)
-
-    # write them to csv
-    df = pd.DataFrame.from_dict(results, orient='index')
-    df.to_csv('../../results/outliers/gb_hourly_imputed_with_outliers.csv')
-
-    # plot
-    baseline_ml.plot_predictions()
+    df.to_csv('../../results/outliers/gb_with_outliers.csv')
 
 
 if __name__ == '__main__':
-    removing_upper_outliers()
-    removing_all_outliers()
-    keep_outliers()
+    no_outliers()
+    with_outliers()
