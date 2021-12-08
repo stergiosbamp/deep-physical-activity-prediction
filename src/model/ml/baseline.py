ba5097cf69840a278579591b7acf97d0daeefba5
stergiosbamp <bampakis.stergios@gmail.com>
2021-12-08 22:03:14 +0200

    baseline.py uses MLEvaluator

--- src/model/ml/baseline.py
+++ src/model/ml/baseline.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.model_selection import TimeSeriesSplit, GridSearchCV
 
 from src.preprocessing.dataset import DatasetBuilder
+from src.model.ml.evaluate import MLEvaluator
 
 
 class BaselineModel:
@@ -22,47 +23,7 @@ class BaselineModel:
 
         self.pipe = make_pipeline(MinMaxScaler(), regressor)
 
-        self.y_pred = None
-        self.y_pred_train = None
-        self.y_pred_val = None
-
-        self.scores_test = dict()
-        self.scores_train = dict()
-        self.scores_val = dict()
-
-    def evaluate_test(self):
-        self.y_pred = self.pipe.predict(self.x_test)
-
-        self.scores_test['R2'] = r2_score(self.y_test, self.y_pred)
-        self.scores_test['MAE'] = mean_absolute_error(self.y_test, self.y_pred)
-        self.scores_test['MAPE'] = mean_absolute_percentage_error(self.y_test, self.y_pred)
-        self.scores_test['MdAE'] = median_absolute_error(self.y_test, self.y_pred)
-        self.scores_test['RMSE'] = mean_squared_error(self.y_test, self.y_pred, squared=False)
-
-        return self.scores_test
-
-    def evaluate_train(self):
-        self.y_pred_train = self.pipe.predict(self.x_train)
-
-        self.scores_train['R2'] = r2_score(self.y_train, self.y_pred_train)
-        self.scores_train['MAE'] = mean_absolute_error(self.y_train, self.y_pred_train)
-        self.scores_train['MAPE'] = mean_absolute_percentage_error(self.y_train, self.y_pred_train)
-        self.scores_train['MdAE'] = median_absolute_error(self.y_train, self.y_pred_train)
-        self.scores_train['RMSE'] = mean_squared_error(self.y_train, self.y_pred_train, squared=False)
-
-        return self.scores_train
-
-    def evaluate_val(self):
-        self.y_pred_val = self.pipe.predict(self.x_val)
-
-        # Now use predictions with any metric from sklearn
-        self.scores_val['R2'] = r2_score(self.y_val, self.y_pred_val)
-        self.scores_val['MAE'] = mean_absolute_error(self.y_val, self.y_pred_val)
-        self.scores_val['MAPE'] = mean_absolute_percentage_error(self.y_val, self.y_pred_val)
-        self.scores_val['MdAE'] = median_absolute_error(self.y_val, self.y_pred_val)
-        self.scores_val['RMSE'] = mean_squared_error(self.y_val, self.y_pred_val, squared=False)
-
-        return self.scores_val
+        self.evaluator = MLEvaluator(x_train, x_val, x_test, y_train, y_val, y_test, self.pipe)
 
     def train_model(self):
         self.pipe.fit(self.x_train, self.y_train)
@@ -71,32 +32,11 @@ class BaselineModel:
         tscv = TimeSeriesSplit(n_splits=5)
         grid = GridSearchCV(self.pipe, grid_params, scoring='neg_median_absolute_error', cv=tscv, n_jobs=-1)
         grid.fit(X, y)
+
         self.pipe = grid.best_estimator_
+        self.evaluator.regressor = self.pipe
+
         print("Best tuned pipeline: {}".format(self.pipe))
-
-    def plot_predictions(self, smooth=False):
-        x_range = self.y_test.index
-        if smooth:
-            df_preds = pd.DataFrame(self.y_pred)
-            df_trues = pd.DataFrame(self.y_test)
-            self.y_pred = df_preds.rolling(200).mean().values
-            self.y_test = df_trues.rolling(200).mean().values
-        plt.plot(x_range, self.y_test, label='true')
-        plt.plot(x_range, self.y_pred, label='pred')
-        plt.legend()
-        plt.show()
-
-    def plot_predictions_train(self, smooth=False):
-        x_range = self.y_train.index
-        if smooth:
-            df_preds = pd.DataFrame(self.y_pred_train)
-            df_trues = pd.DataFrame(self.y_train)
-            self.y_pred_train = df_preds.rolling(200).mean().values
-            self.y_train = df_trues.rolling(200).mean().values
-        plt.plot(x_range, self.y_train, label='true')
-        plt.plot(x_range, self.y_pred_train, label='pred')
-        plt.legend()
-        plt.show()
 
     def set_pipe(self, new_pipe):
         self.pipe = new_pipe
@@ -106,7 +46,7 @@ if __name__ == '__main__':
     dataset_builder = DatasetBuilder(n_in=3*24,
                                      granularity='whatever',
                                      save_dataset=True,
-                                     directory='../../../data/datasets/variations/df-3x24-no-wear-days-500-just-steps.pkl',
+                                     directory='../../../data/datasets/variations/df-3x24-no-zeros-just-steps.pkl',
                                      total_users=None)
 
     dataset = dataset_builder.create_dataset_all_features()
@@ -125,13 +65,13 @@ if __name__ == '__main__':
 
     baseline_ml.tune_model(X=x_train_val, y=y_train_val, grid_params=grid)
 
-    scores_train = baseline_ml.evaluate_train()
-    scores_val = baseline_ml.evaluate_val()
-    scores_test = baseline_ml.evaluate_test()
+    scores_train = baseline_ml.evaluator.evaluate_train()
+    scores_val = baseline_ml.evaluator.evaluate_val()
+    scores_test = baseline_ml.evaluator.evaluate_test()
 
     print("Train set scores:", scores_train)
     print("Val set scores:", scores_val)
     print("Test set scores:", scores_test)
 
-    baseline_ml.plot_predictions_train(smooth=True)
-    baseline_ml.plot_predictions(smooth=True)
+    baseline_ml.evaluator.plot_predictions_train(smooth=True)
+    baseline_ml.evaluator.plot_predictions(smooth=True)
