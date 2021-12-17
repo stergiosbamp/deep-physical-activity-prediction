eab573209c9e577900f239b5c6e04773d3a94b1b
stergiosbamp <bampakis.stergios@gmail.com>
2021-12-17 19:01:55 +0200

    Refactored all experiments with the new class of BaselineML
    Upload results for features experiments with steps+date features
    
    Refactored all experiments with the new class of BaselineML

--- src/experiments/daily_sources.py
+++ src/experiments/daily_sources.py
@@ -9,7 +9,7 @@ from src.preprocessing.dataset import DatasetBuilder
 
 
 def with_daily_sources():
-    pipe = make_pipeline(MinMaxScaler(), GradientBoostingRegressor(verbose=1, random_state=1))
+    gb_regressor = GradientBoostingRegressor(verbose=1, random_state=1)
 
     ds_builder = DatasetBuilder(n_in=3*24,
                                 granularity='whatever',
@@ -19,12 +19,12 @@ def with_daily_sources():
     dataset = ds_builder.create_dataset_all_features()
     X_train, X_test, y_train, y_test = ds_builder.get_train_test(dataset=dataset)
 
-    baseline_ml = BaselineModel(X_train, X_test, y_train, y_test)
-    baseline_ml.set_pipe(pipe)
+    baseline_ml = BaselineModel(X_train, None, X_test, y_train, None, y_test, gb_regressor)
+    baseline_ml.evaluator.zero_preds = False
 
     # record
-    results = baseline_ml.score()
-    print(results)
+    baseline_ml.train_model()
+    results = baseline_ml.evaluator.evaluate_test()
 
     # write them to csv
     df = pd.DataFrame.from_dict(results, orient='index')
@@ -32,7 +32,7 @@ def with_daily_sources():
 
 
 def without_daily_sources():
-    pipe = make_pipeline(MinMaxScaler(), GradientBoostingRegressor(verbose=1, random_state=1))
+    gb_regressor = GradientBoostingRegressor(verbose=1, random_state=1)
 
     ds_builder = DatasetBuilder(n_in=3*24,
                                 granularity='whatever',
@@ -42,11 +42,12 @@ def without_daily_sources():
     dataset = ds_builder.create_dataset_all_features()
     X_train, X_test, y_train, y_test = ds_builder.get_train_test(dataset=dataset)
 
-    baseline_ml = BaselineModel(X_train, X_test, y_train, y_test)
-    baseline_ml.set_pipe(pipe)
+    baseline_ml = BaselineModel(X_train, None, X_test, y_train, None, y_test, gb_regressor)
+    baseline_ml.evaluator.zero_preds = False
 
     # record
-    results = baseline_ml.score()
+    baseline_ml.train_model()
+    results = baseline_ml.evaluator.evaluate_test()
     print(results)
 
     # # write them to csv
