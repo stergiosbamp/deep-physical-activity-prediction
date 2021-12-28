963c4bd4a2bbe360595e7e5cb3f061bf29447979
stergiosbamp <bampakis.stergios@gmail.com>
2021-12-28 19:43:53 +0200

    Initial 'ubiwear' library in top-level directory

---
+++ ubiwear/README.md
@@ -0,0 +1,8 @@
+# UBIWEAR
+
+A Python library for pre-processing ubiquitous aggregated self-tracking data.
+
+It offers ready-to-easy ML and DL models to be applied right on your time-series problem.
+
+## Usage
+
---
+++ ubiwear/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='ubiwear-stergiosbamp',
+    version='',
+    url='https://github.com/stergiosbamp/deep-physical-activity-prediction',
+    license='',
+    author='Stergios Bampakis',
+    author_email='bampakis.stergios@gmail.com',
+    python_requires='>=3.8',
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent'
+    ],
+    install_requires=['pandas', 'scikit-learn'],
+    package_dir={'ubiwear': 'src'},
+    packages=[''],
+    description='A Python library for pre-processing ubiquitous aggregated self-tracking data'
+)
