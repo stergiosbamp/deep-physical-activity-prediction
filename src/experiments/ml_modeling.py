a33ab75bf81affa12ae1739bfafa7e7471e61e44
stergiosbamp <bampakis.stergios@gmail.com>
2022-01-22 17:15:38 +0200

    Refactored ML modeling and upload new resuls with scaling target

--- src/experiments/ml_modeling.py
+++ src/experiments/ml_modeling.py
@@ -9,73 +9,10 @@ from src.model.ml.evaluator import MLEvaluator
 from src.preprocessing.dataset import DatasetBuilder
 
 
-def trees():
-    trees_grid = {
-        "decisiontreeregressor__criterion": ["mse", "friedman_mse"],
-        "decisiontreeregressor__max_depth": [5, 10, 15, 20],
-    }
-
-    trees_regressor = DecisionTreeRegressor(random_state=1)
-
-    baseline_ml = BaselineModel(x_train=x_train, x_val=x_val, x_test=x_test,
-                                y_train=y_train, y_val=y_val, y_test=y_test,
-                                regressor=trees_regressor)
-
-    baseline_ml.tune_model(X=x_train_val, y=y_train_val, grid_params=trees_grid,
-                           path_to_save="../model/ml/models/tree.pkl")
-
-    scores_train = baseline_ml.evaluator.evaluate_train()
-    scores_val = baseline_ml.evaluator.evaluate_val()
-    scores_test = baseline_ml.evaluator.evaluate_test()
-
-    print("Train set scores:", scores_train)
-    print("Val set scores:", scores_val)
-    print("Test set scores:", scores_test)
-
-    baseline_ml.evaluator.save_results(scores_train, "trees-train.csv")
-    baseline_ml.evaluator.save_results(scores_val, "trees-val.csv")
-    baseline_ml.evaluator.save_results(scores_test, "trees-test.csv")
-
-    baseline_ml.evaluator.plot_predictions_train(smooth=True)
-    baseline_ml.evaluator.plot_predictions(smooth=True)
-
-
-def boosting():
-    gb_grid = {
-        "histgradientboostingregressor__loss": ['least_squares', 'poisson'],
-        "histgradientboostingregressor__l2_regularization": [0, 1],
-        "histgradientboostingregressor__max_iter": [100, 200, 300],
-    }
-
-    gb_regressor = HistGradientBoostingRegressor(random_state=1)
-
-    baseline_ml = BaselineModel(x_train=x_train, x_val=x_val, x_test=x_test,
-                                y_train=y_train, y_val=y_val, y_test=y_test,
-                                regressor=gb_regressor)
-
-    baseline_ml.tune_model(X=x_train_val, y=y_train_val, grid_params=gb_grid,
-                           path_to_save="../model/ml/models/gb.pkl")
-
-    scores_train = baseline_ml.evaluator.evaluate_train()
-    scores_val = baseline_ml.evaluator.evaluate_val()
-    scores_test = baseline_ml.evaluator.evaluate_test()
-
-    print("Train set scores:", scores_train)
-    print("Val set scores:", scores_val)
-    print("Test set scores:", scores_test)
-
-    baseline_ml.evaluator.save_results(scores_train, "gb-train.csv")
-    baseline_ml.evaluator.save_results(scores_val, "gb-val.csv")
-    baseline_ml.evaluator.save_results(scores_test, "gb-test.csv")
-
-    baseline_ml.evaluator.plot_predictions_train(smooth=True)
-    baseline_ml.evaluator.plot_predictions(smooth=True)
-
-
-if __name__ == '__main__':
+def run_ml_model(regressor, grid, model_name):
     # dataset
     dataset_builder = DatasetBuilder(n_in=3*24,
-                                     granularity='whatever',
+                                     granularity='1H',
                                      save_dataset=True,
                                      directory='../../data/datasets/hourly/df-3x24-just-steps.pkl',
                                      total_users=None)
@@ -98,31 +35,76 @@ if __name__ == '__main__':
     y_test = target_scaler.transform(y_test.values.reshape(-1, 1))
 
     # modeling
+    baseline_ml = BaselineModel(regressor=regressor)
+
+    # choose between simple train or cross-validate
+    # model = baseline_ml.train_model(x=x_train, y=y_train)
+    model = baseline_ml.tune_model(x=x_train_val, y=y_train_val.reshape(-1), grid_params=grid)
+
+    evaluator = MLEvaluator(model)
+
+    # inverse transformation for performance recording
+    y_pred = evaluator.inference(x_test)
+    y_pred_train = evaluator.inference(x_train)
+    y_pred_val = evaluator.inference(x_val)
+
+    y_pred = target_scaler.inverse_transform(y_pred.reshape(-1, 1))
+    y_pred_train = target_scaler.inverse_transform(y_pred_train.reshape(-1, 1))
+    y_pred_val = target_scaler.inverse_transform(y_pred_val.reshape(-1, 1))
+
+    y_test = target_scaler.inverse_transform(y_test)
+    y_val = target_scaler.inverse_transform(y_val)
+    y_train = target_scaler.inverse_transform(y_train)
+
+    scores_test = evaluator.evaluate(y_test, y_pred)
+    scores_train = evaluator.evaluate(y_train, y_pred_train)
+    scores_val = evaluator.evaluate(y_val, y_pred_val)
+
+    print("Train set scores:", scores_train)
+    print("Val set scores:", scores_val)
+    print("Test set scores:", scores_test)
+
+    evaluator.save_results(scores_train, '{}-train.csv'.format(model_name))
+    evaluator.save_results(scores_val, '{}-val.csv'.format(model_name))
+    evaluator.save_results(scores_test, '{}-test.csv'.format(model_name))
+
+
+if __name__ == '__main__':
+    MODELS = [
+        Ridge(random_state=1),
+        DecisionTreeRegressor(random_state=1),
+        HistGradientBoostingRegressor(random_state=1)
+    ]
+
+    # Parameters for hyper-tuning
     ridge_grid = {
         "ridge__alpha": [0.5, 1.0, 2.0, 5.0],
         "ridge__tol": [1e-3, 1e-4, 1e-5],
         "ridge__solver": ["auto"]
     }
-    ridge_regressor = Ridge(random_state=1)
 
-    baseline_ml = BaselineModel(regressor=ridge_regressor)
+    trees_grid = {
+        "decisiontreeregressor__criterion": ["mse", "friedman_mse"],
+        "decisiontreeregressor__max_depth": [5, 10, 15, 20],
+    }
 
-    # choose between simple train or cross-validate
-    # model = baseline_ml.train_model(x=x_train, y=y_train)
-    model = baseline_ml.tune_model(x=x_train_val, y=y_train_val, grid_params=ridge_grid)
+    gb_grid = {
+        "histgradientboostingregressor__loss": ['least_squares', 'poisson'],
+        "histgradientboostingregressor__l2_regularization": [0, 1],
+        "histgradientboostingregressor__max_iter": [100, 200, 300],
+    }
 
-    evaluator = MLEvaluator(model)
+    GRID_PARAMS = [
+        ridge_grid,
+        trees_grid,
+        gb_grid
+    ]
 
-    y_pred = evaluator.inference(x_test)
+    MODEL_NAMES = [
+        'ridge',
+        'trees',
+        'gb'
+    ]
 
-    # inverse transform for performance recording
-    y_test = target_scaler.inverse_transform(y_test)
-    y_pred = target_scaler.inverse_transform(y_pred)
-
-    # scores_train = baseline_ml.evaluator.evaluate(x_train, y_train)
-    # scores_val = baseline_ml.evaluator.evaluate(x_val, y_val)
-    scores_test = evaluator.evaluate(y_test, y_pred)
-
-    # print("Train set scores:", scores_train)
-    # print("Val set scores:", scores_val)
-    print("Test set scores:", scores_test)
+    for model, grid, model_name in zip(MODELS, GRID_PARAMS, MODEL_NAMES):
+        run_ml_model(model, grid, model_name)
